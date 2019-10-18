4899fff45896af746b027a70ee7ed762f0135e59
Stel312 <nstelken312@gmail.com>
2019-10-18 13:31:06 -0500

    changed reference

--- src/main/java/com/Stel/SpawnerMob/common/lib/Reference.java
+++ src/main/java/com/Stel/SpawnerMob/common/lib/Reference.java
@@ -4,8 +4,6 @@ public class Reference {
         public static final String MODID = "spawnermob";
         public static final String MODNAME = "Spawner Mob";
         public static final String MODVER = "0.1";
-        public static final String MCVER = "1.12.x";
-        public static final String MODCHANNEL = MODID;
         public static final String CLIENTPROXY = "com.Stel.SpawnerMob.client.core.proxy.ClientProxy";
         public static final String COMMONPROXY = "com.Stel.SpawnerMob.common.core.proxy.CommonProxy";
 }
